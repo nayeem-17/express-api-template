a75cf7251ee0c6e462199e05af1927903c079ce9
nayeem-17 <nasa.28apr@gmail.com>
2022-01-22 18:27:08 +0600

    pool updated

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -1,19 +1,19 @@
-import { Connection, getConnection } from 'oracledb';
+import { Connection, createPool } from 'oracledb';
 // require('dotenv').config({ path: '../.env' });
 let connection: Connection;
-async function createConnection() {
+const createConnection = async () => {
   try {
-    const connection = await getConnection({
+    const pool = await createPool({
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       connectString: process.env.DB_CONNECTION_STRING,
     });
     console.log('Database connected');
-    return connection;
+    return pool.getConnection();
   } catch (error) {
     console.log('error occurred while connecting to database' + error);
     console.error(error);
   }
-}
+};
 
 export { createConnection, connection };
